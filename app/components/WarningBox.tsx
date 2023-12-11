c16e200351f4b919d20e63b94f2bc801df5369af
abdullahtalhacan <abdullahcanmail@gmail.com>
2023-12-11 15:58:34 +0400

    Tekara duzenleme yapılacak

--- app/components/WarningBox.tsx
+++ app/components/WarningBox.tsx
@@ -10,7 +10,7 @@ const WarningBox = () => {
               <div className="absolute right-0 top-0 flex items-center justify-center rounded-lg">
                   {
                       toggle ? (
-                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" className="w-5 h-5 text-green-400"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd"></path></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" className="w-5 h-5 text-green-400"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd"></path></svg>
                       ) : (
                         <span className="w-5 h-5 text-center text-red-400">*</span>
                       )
