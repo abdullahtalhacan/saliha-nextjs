1bb4164f2a755815e2aa1667f1fd082d2ed169b2
abdullahtalhacan <abdullahcanmail@gmail.com>
2023-12-17 16:24:01 +0400

    test

--- app/components/LogPanel.tsx
+++ app/components/LogPanel.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const LogPanel = ({...rest}) => {
   const restData = JSON.stringify(rest).substring(1).slice(0, -1).split(',')
   return (
-    <div className='fixed top-32 left-0 px-2 py-2 z-50 bg-zinc-900/30 flex flex-col space-y-2'>
+    <div className='hidden fixed top-32 left-0 px-2 py-2 z-50 bg-zinc-900/30 flex flex-col space-y-2'>
         {Object.keys(restData).map((_, index:number) => (
             <div key={index}>{restData[index]}</div>
         ))}
